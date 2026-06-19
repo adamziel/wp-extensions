f5f11f1e78cfcb03bdb2882a37975051f9a23de9
CAO Supervisor <supervisor@example.local>
2026-06-19 18:00:44 +0200

    Release Pure PHP FTS Indexer 0.1.6

--- indexer/indexer.php
+++ indexer/indexer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Pure PHP FTS Indexer
  * Description: HTML-aware pure-PHP full-text search engine for WordPress.
- * Version: 0.1.5
+ * Version: 0.1.6
  * Requires PHP: 8.1
  */
 
